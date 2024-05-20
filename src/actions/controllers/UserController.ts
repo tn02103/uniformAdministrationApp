039ef924ee522b99099deb24191c9a12fe77e80a
Tom Nöller <tn02103@gmx.de>
2024-05-20 13:52:52 +0200

    added neccessary ServerActions

--- src/actions/controllers/UserController.ts
+++ src/actions/controllers/UserController.ts
@@ -1,6 +1,12 @@
 "use server";
 
 import { AuthRole } from "@/lib/AuthRoles";
+import { prisma } from "@/lib/db";
+import { nameValidationPattern, passwordValidationPattern, userNameValidationPattern, uuidValidationPattern } from "@/lib/validations";
+import { User } from "@/types/userTypes";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt';
+import { revalidatePath } from "next/cache";
 import { UserDBHandler } from "../dbHandlers/UserDBHandler";
 import { genericSAValidatiorV2 } from "../validations";
 
@@ -8,6 +14,59 @@ const dbHandler = new UserDBHandler();
 
 export const getUserList = () => genericSAValidatiorV2(
     AuthRole.admin,
-    true,{}
-).then(({assosiation}) => dbHandler.getUsersList(assosiation));
+    true, {}
+).then(({ assosiation }) => dbHandler.getUsersList(assosiation));
 
+export const createUser = (data: { username: string, name: string, role: AuthRole, active: boolean }, password: string) => genericSAValidatiorV2(
+    AuthRole.admin,
+    (userNameValidationPattern.test(data.username)
+        && nameValidationPattern.test(data.name)
+        && (typeof data.active === "boolean")
+        && (data.role in AuthRole && typeof data.role === 'number')),
+    {}
+).then(async ({ assosiation }) => prisma.$transaction(async (client) => {
+    const userList = await dbHandler.getUsersList(assosiation, client as PrismaClient);
+
+    if (userList.find(u => u.username === data.username)) {
+        throw new Error("Could not save data Username allready in use");
+    }
+
+    await dbHandler.create(
+        data,
+        assosiation,
+        await bcrypt.hash(password, 12),
+        client as PrismaClient
+    );
+    revalidatePath(`/[locale]/${assosiation}/admin/users`, 'page');
+}));
+
+export const updateUser = (data: User) => genericSAValidatiorV2(
+    AuthRole.admin,
+    (uuidValidationPattern.test(data.id)
+        && userNameValidationPattern.test(data.username)
+        && nameValidationPattern.test(data.name)
+        && (typeof data.active === "boolean")
+        && (data.role in AuthRole && typeof data.role === 'number')),
+    { userId: data.id }
+).then(async ({ assosiation }) => {
+    await dbHandler.update(data.id, data.name, data.role, data.active, prisma)
+    revalidatePath(`/[locale]/${assosiation}/admin/users`, 'page');
+});
+
+export const changeUserPassword = (userId: string, password: string) => genericSAValidatiorV2(
+    AuthRole.admin,
+    uuidValidationPattern.test(userId) && passwordValidationPattern.test(password),
+    { userId }
+).then(async () => prisma.$transaction([
+    dbHandler.setPassword(userId, await bcrypt.hash(password, 12), prisma),
+    dbHandler.removeRefreshToken(userId, prisma),
+]));
+
+export const deleteUser = (userId: string) => genericSAValidatiorV2(
+    AuthRole.admin,
+    uuidValidationPattern.test(userId),
+    { userId }
+).then(async ({ assosiation }) => {
+    await dbHandler.delete(userId, prisma);
+    revalidatePath(`/[locale]/${assosiation}/admin/users`, 'page');
+});
--- src/actions/dbHandlers/UserDBHandler.ts
+++ src/actions/dbHandlers/UserDBHandler.ts
@@ -1,12 +1,52 @@
+import { AuthRole } from "@/lib/AuthRoles";
 import { prisma } from "@/lib/db";
 import { userArgs } from "@/types/userTypes";
 import { PrismaClient } from "@prisma/client";
 
 export class UserDBHandler {
 
-    getUsersList = (fk_assosiation: string, client?: PrismaClient) => 
-        (client??prisma).user.findMany({
-            where: {fk_assosiation},
+    getUsersList = (fk_assosiation: string, client?: PrismaClient) =>
+        (client ?? prisma).user.findMany({
+            where: { fk_assosiation },
             ...userArgs,
+        });
+
+    create = (data: { username: string, name: string, role: AuthRole, active: boolean }, fk_assosiation: string, password: string, client: PrismaClient) =>
+        client.user.create({
+            data: {
+                username: data.username,
+                name: data.name,
+                role: data.role,
+                active: data.active,
+                password,
+                assosiation: { connect: { id: fk_assosiation } },
+            }
+        });
+
+    update = (id: string, name: string, role: AuthRole, active: boolean, client: PrismaClient) =>
+        client.user.update({
+            where: { id },
+            data: {
+                name,
+                role,
+                active,
+                failedLoginCount: 0,
+            }
+        });
+
+    setPassword = (id: string, password: string, client: PrismaClient) =>
+        client.user.update({
+            where: { id },
+            data: { password }
+        });
+
+    removeRefreshToken = (fk_user: string, client: PrismaClient) =>
+        client.refreshToken.deleteMany({
+            where: { fk_user }
+        });
+
+    delete = (id: string, client: PrismaClient) =>
+        client.user.delete({
+            where: { id }
         });
 }
