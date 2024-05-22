3c73f10308ae950e61b715ae13c0251a4a1fb419
Tom Nöller <tn02103@gmx.de>
2024-05-22 20:39:53 +0200

    fixed bug db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,13 @@
 
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs, Omit } from "@prisma/client/runtime/library";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
