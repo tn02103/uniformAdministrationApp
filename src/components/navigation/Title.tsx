9542f520dd98715e8795500a8750115f67a80c26
dion <dion@tschoepe.dev>
2024-10-30 22:14:12 +0100

    added missing type

--- src/components/navigation/Title.tsx
+++ src/components/navigation/Title.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Title = ({ text }) => {
+const Title = ({ text }: { text: string }) => {
     return (
-        <div className='d-flex align-items-center p-4 ps-0 pe-3' style={{width: 'min-content', height: 60 }}>
+        <div className='d-flex align-items-center justify-content-center p-3' style={{width: 'fit-content', height: 60 }}>
             <h3 className='m-0'>{text}</h3>
         </div>
     );
