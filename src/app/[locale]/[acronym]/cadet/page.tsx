c9dff65de944f689b89a8830874d95cbd50abb44
Tom N. <tn02103@gmx.de>
2024-11-04 20:07:11 +0100

    created filter for Personal overview during active Inspections

--- src/app/[locale]/[acronym]/cadet/page.tsx
+++ src/app/[locale]/[acronym]/cadet/page.tsx
@@ -4,16 +4,21 @@ import GeneralOverviewTable from "./table";
 
 const CadetListPage = async ({ searchParams }: any) => {
     const t = await getI18n();
-    const orderBy = searchParams.orderBy === "firstname" ? "firstname" : "lastname";
-    const asc = searchParams.asc ? searchParams.asc === "true" ? true : false : true;
+    const data = await getPersonnelListData({
+        orderBy: searchParams.orderBy === "firstname" ? "firstname" : "lastname",
+        asc: searchParams.asc ? searchParams.asc === "true" ? true : false : true,
+        include: {
+            deregistered: !!searchParams.deregistered,
+            inspected: !!searchParams.inspected,
+        }
+    });
 
     return (
         <div className="container-lg content-center bg-light rounded px-md-3 px-xl-5 p-0">
             <div className="row pt-2 pb-2 m-0">
                 <h1 data-testid="div_cadetListHeader" className="text-center">{t('generalOverview.header')}</h1>
             </div>
-            <GeneralOverviewTable data={await getPersonnelListData(orderBy, asc)} />
-
+            <GeneralOverviewTable data={data} />
         </div>
     )
 }
