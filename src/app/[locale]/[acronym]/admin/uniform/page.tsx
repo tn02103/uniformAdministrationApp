223203dae0d21ee3e9d6aac706a7ebe632b0ce78
Tom Nöller <tn02103@gmx.de>
2025-04-12 11:10:00 +0200

    ..

--- src/app/[locale]/[acronym]/admin/uniform/page.tsx
+++ src/app/[locale]/[acronym]/admin/uniform/page.tsx
@@ -8,9 +8,7 @@ import { UniformTypeTable } from "./_typeAdministration/UniformTypeTable";
 export default async function UniformAdminPage() {
     const t = await getI18n();
     const typeList = await getUniformTypeList();
-
-
-    
+   
     return (
         <div className="container-xl content-center bg-light rounded">
             <h1 className="text-center">
