da9bf06a6248bee1af63092c72caf6437c520ce1
tn02103 <tn02103@gmx.de>
2025-07-05 13:05:10 +0200

    trying two workers for pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 2,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* amount of allowed failures */
     maxFailures: 5,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
