1fed7b09017510c28468866b4a4dde4f2e97cb54
Tom Nöller <tn02103@gmx.de>
2025-08-24 18:19:09 +0200

    fixed window.location problem

--- jest/helpers/test-utils.tsx
+++ jest/helpers/test-utils.tsx
@@ -1,31 +1,3 @@
-import { render, RenderOptions } from '@testing-library/react';
-import { ReactElement } from 'react';
-
-/**
- * Custom render function for components that need providers
- * You can extend this with providers like QueryClient, Router, etc.
- */
-export function renderWithProviders(
-    ui: ReactElement,
-    options?: Omit<RenderOptions, 'wrapper'>
-) {
-    // Add providers here when needed
-    // const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
-    //   return (
-    //     <QueryClientProvider client={testQueryClient}>
-    //       <Router>
-    //         {children}
-    //       </Router>
-    //     </QueryClientProvider>
-    //   );
-    // };
-
-    return render(ui, { ...options });
-}
-
-
-
-
 
 export const checkDateTolerance = (date: Date) => {
     const now = new Date();
