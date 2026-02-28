d002a6eca5974ece8ea659922014d2e5f6bdbcb0
Tom Nöller <tn02103@gmx.de>
2026-02-28 16:43:23 +0100

    added debug message

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { defineConfig } from "prisma/config";
 
+console.debug("Prisma configuration loaded with DATABASE_URL:", process.env.DATABASE_URL);
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: { 
