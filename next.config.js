e27628003609ac2f5b4017ac63718932f5563af0
tn02103 <tn02103@gmx.de>
2026-03-29 18:45:43 +0200

    fixed problems to use nextjs 16

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: "build",
-    eslint: {
-        dirs: ["src", "tests"],
-        ignoreDuringBuilds: true,
-    },
 }
 
 export default nextConfig;
