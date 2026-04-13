03c3a492f454010366deb1529bbe4640aa5fa63b
Tom Nöller <tn02103@gmx.de>
2026-04-13 19:44:03 +0200

    fix(eslint): eslint setup from broken merge

--- tests/e2e/admin/uniform/size.spec.ts
+++ tests/e2e/admin/uniform/size.spec.ts
@@ -42,11 +42,13 @@ test.describe('Uniform size configuration', () => {
         await expect(divList).toHaveCount(sizes.length);
 
         const promises = [];
+        /* eslint-disable playwright/missing-playwright-await */
         for (let i = 0; i < sizes.length; i++) {
             promises.push(expect.soft(divList.nth(i)).toHaveAttribute("data-testid", `div_size_${sizes[i].id}`));
             promises.push(expect.soft((await uniformSizePage.div_name(sizes[i].id))).toHaveText(sizes[i].name));
             promises.push(expect.soft(uniformSizePage.div_index(sizes[i].id)).toHaveText(String(i + 1)));
         }
+        /* eslint-enable playwright/missing-playwright-await */
         
         await Promise.all(promises);
     });
