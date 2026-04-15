b20def0320b43394f5bdc8936526463f78d4d6aa
Tom Nöller <tn02103@gmx.de>
2026-04-16 00:11:52 +0200

    added missing headers to rewrite

--- src/proxy.ts
+++ src/proxy.ts
@@ -18,9 +18,11 @@ export async function proxy(request: NextRequest) {
     if (response.status === 307) {
         return response;
     }
-
+    
+    const pathname = request.nextUrl.pathname;
+    const pathnameParts = pathname.substring(1).split("/");
     const session = await getIronSession();
-
+   
     // Redirect root to /app or /login since no homepage is implemented
     if (request.nextUrl.pathname.length < 4) {
         if (!session.user) {
@@ -30,26 +32,24 @@ export async function proxy(request: NextRequest) {
     }
 
     // Check if the path is unsecured (e.g. /api/*, /*/login, /*/docs/*)
-    const pathname = request.nextUrl.pathname;
-    const pathnameParts = pathname.substring(1).split("/");
     if (unsecuredPaths.some(parts => parts.every((part, index) => !part || pathnameParts[index] === part))) {
         return response;
     }
 
     // For all other paths, check if the user is authenticated
     if (!session.user) {
-        return NextResponse.redirect(new URL('/login', request.url));
+         return NextResponse.redirect(new URL('/login', request.url));
     }
 
     const rewrite = {
         required: false,
-        url: pathname,
+        url: request.url,
     };
     // rewrite /app/* to /:acronym/* to scope all app routes to the user's acronym
     if (pathnameParts[1] === "app") {
         rewrite.required = true;
         rewrite.url = rewrite.url.replace('app', session.user.acronym);
-    }
+   }
 
     // rewrite /uniform/list to /uniform/list/null to correct for missing optional parameter in the route definition
     if (pathnameParts[2] === "uniform" && pathnameParts[3] === "list" && pathnameParts.length === 4) {
@@ -59,7 +59,7 @@ export async function proxy(request: NextRequest) {
 
     // rewrite if necessary, otherwise return the original response
     if (rewrite.required) {
-        return NextResponse.rewrite(new URL(rewrite.url, request.url));
+        return NextResponse.rewrite(new URL(rewrite.url, request.url), { headers: response.headers });
     }
 
     return response;
