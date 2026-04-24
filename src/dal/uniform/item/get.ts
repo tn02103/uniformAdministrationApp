1a9391bab83f64814cd6fab91a655b9c181fcd23
Tom Nöller <tn02103@gmx.de>
2026-04-24 16:50:35 +0200

    fixed indirect bug from merge-request

--- src/dal/uniform/item/get.ts
+++ src/dal/uniform/item/get.ts
@@ -53,18 +53,15 @@ export const getItemLabels = async (): Promise<ItemLabel[]> => genericSANoDataVa
                 cadet: true,
             },
         },
-        uniformDeficiencies: {
+        deficiencies: {
             where: {
-                deficiency: {
-                    dateResolved: null,
+                dateResolved: null,
+                type: {
+                    dependent: "uniform",
                 },
             },
             include: {
-                deficiency: {
-                    include: {
-                        type: true,
-                    },
-                },
+                type: true,
             },
         },
     },
@@ -91,9 +88,9 @@ export const getItemLabels = async (): Promise<ItemLabel[]> => genericSANoDataVa
         id: item.storageUnit.id,
         name: item.storageUnit.name,
     } : null,
-    activeDeficiencies: item.uniformDeficiencies.map(ud => ({
-        typeName: ud.deficiency.type.name,
-        comment: ud.deficiency.comment,
+    activeDeficiencies: item.deficiencies.map(ud => ({
+        typeName: ud.type.name,
+        comment: ud.comment,
     })),
 })));
 
